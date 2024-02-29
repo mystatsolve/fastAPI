75a751fa89b3e415cfcac75148430a5a82e6cf43
mystatsolve <mystatsolve@gmail.com>
2024-02-29 23:35:57 +0900

    1 commit

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -9,4 +9,9 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("/index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/concat_words")
+async def concat_words(request: Request, word1: str = Form(...), word2: str = Form(...)):
+    concatenated_words = word1 + word2
+    return templates.TemplateResponse("index.html", {"request": request, "result": concatenated_words})
